33ea8eed3840c3c25451f431deaca3157b9baf25
Manuele Menozzi <manuele.menozzi@gmail.com>
2014-05-01 18:48:42 +0200

    Adds indexing to queue

--- src/Model/Indexer.php
+++ src/Model/Indexer.php
@@ -7,6 +7,23 @@ class Webgriffe_IndexQueue_Model_Indexer extends Mage_Index_Model_Indexer
 {
     public function processEntityAction(Varien_Object $entity, $entityType, $eventType)
     {
-        return parent::processEntityAction($entity, $entityType, $eventType);
+        //return parent::processEntityAction($entity, $entityType, $eventType);
+
+        /** @var Lilmuckers_Queue_Helper_Data $lilqueueHelper */
+        $lilqueueHelper = Mage::helper('lilqueue');
+        /** @var Lilmuckers_Queue_Model_Queue_Abstract $indexQueue */
+        $indexQueue = $lilqueueHelper->getQueue('indexQueue');
+        $indexTask = $lilqueueHelper->createTask(
+            'indexTask',
+            array(
+                'entity' => $entity->getData(),
+                'entityType' => $entityType,
+                'eventType' => $eventType,
+                'allowTableChanges' => $this->_allowTableChanges,
+                'isObjectNew' => method_exists ($entity, 'isObjectNew') ? $entity->isObjectNew() : false,
+            )
+        );
+        $indexQueue->addTask($indexTask);
+        return $this;
     }
 }
--- src/Test/Model/Indexer.php
+++ src/Test/Model/Indexer.php
@@ -25,4 +25,46 @@ class Webgriffe_IndexQueue_Test_Model_Indexer extends EcomDev_PHPUnit_Test_Case
         $this->assertTrue(method_exists(new Webgriffe_IndexQueue_Model_Indexer(), 'processEntityAction'));
     }
 
+    public function testThatProcessEntityActionQueuesIndexing()
+    {
+        $queueMock = $this->getMock('Lilmuckers_Queue_Model_Queue_Abstract');
+
+        $lilqueueMock = $this->getMock('Lilmuckers_Queue_Helper_Data');
+        $lilqueueMock
+            ->expects($this->once())
+            ->method('getQueue')
+            ->with('indexQueue')
+            ->will($this->returnValue($queueMock));
+
+        $entity = new Varien_Object(array('my' => 'data'));
+        $entityType = Mage_Catalog_Model_Product::ENTITY;
+        $eventType = Mage_Index_Model_Event::TYPE_SAVE;
+
+        $taskMock = $this->getMock('Lilmuckers_Queue_Model_Queue_Task');
+        $lilqueueMock
+            ->expects($this->once())
+            ->method('createTask')
+            ->with(
+                'indexTask',
+                array(
+                    'entity' => $entity->getData(),
+                    'entityType' => $entityType,
+                    'eventType' => $eventType,
+                    'allowTableChanges' => true,
+                    'isObjectNew' => false,
+                )
+            )
+            ->will($this->returnValue($taskMock));
+
+        $queueMock
+            ->expects($this->once())
+            ->method('addTask')
+            ->with($taskMock);
+
+        $this->replaceByMock('helper', 'lilqueue', $lilqueueMock);
+
+        $indexer = new Webgriffe_IndexQueue_Model_Indexer();
+        $indexer->processEntityAction($entity, $entityType, $eventType);
+    }
+
 } 
