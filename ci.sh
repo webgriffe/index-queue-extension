605b6b15f3dcdf065f7cd315b3aceb24151088a4
Manuele Menozzi <manuele.menozzi@gmail.com>
2014-05-08 14:46:25 +0200

    Moves CI params in a separate script

--- ci.sh
+++ ci.sh
@@ -1,13 +1,8 @@
 #!/bin/sh
 
+source ./params.sh
+
 export magento_dir=magento
-export db_host=127.0.0.1
-export db_user=root
-export db_pass=p4ssw0rd
-export db_name=index-queue-extension-ci
-export db_test_name=${db_name}_test
-export install_sample_data=no
-export base_url=http://index-queue-extension.mage.dev/
 export phpunit_filter=Webgriffe_IndexQueue
 
 export MAGENTO_VERSION=magento-ce-1.8.1.0
