31ca177331b51e44ed5be90ecf4c6db3ae395e20
Manuele Menozzi <manuele.menozzi@gmail.com>
2014-05-08 18:20:45 +0200

    Fix CI script

--- ci.sh
+++ ci.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
 source ./params.sh
+export db_host=${db_host}
+export db_user=${db_user}
+export db_pass=${db_pass}
+export db_name=${db_name}
+export base_url=${base_url}
+export db_test_name=${db_test_name}
+export install_sample_data=${install_sample_data}
 
 export magento_dir=magento
 export phpunit_filter=Webgriffe_IndexQueue
